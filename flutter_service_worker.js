5a7063f7d403c996767539200b6a60bc7738d6e3
AurelioAlfons <yuroalfons0407@gmail.com>
2025-05-08 10:27:48 +1000

    Built web
    Branch: main
    Commit: 1dcead858e120fbf15db7c38aa65d9203ca734fe
    
    package:peanut 6.0.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -24,7 +24,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "40f2ceef4d48966c01df70d7b145f37b
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "aa4d9d14a7ffde9d6b4fb86e52e036dc",
+"flutter_bootstrap.js": "7128f1ac9a525b0e4dea88771cb9a90f",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
